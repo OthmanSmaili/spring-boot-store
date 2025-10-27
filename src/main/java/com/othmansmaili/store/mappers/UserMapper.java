d8e1afa02dbccc4d5624eaa5f392973710b3db21
Othman Smaili <othman.smaili123@gmail.com>
2025-10-26 23:39:17 -0700

    complete CRUD API for products

--- src/main/java/com/othmansmaili/store/mappers/UserMapper.java
+++ src/main/java/com/othmansmaili/store/mappers/UserMapper.java
@@ -1,10 +1,15 @@
 package com.othmansmaili.store.mappers;
 
+import com.othmansmaili.store.dtos.RegisterUserRequest;
+import com.othmansmaili.store.dtos.UpdateUserRequest;
 import com.othmansmaili.store.dtos.UserDto;
 import com.othmansmaili.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toDto(User user);
+    User toEntity(RegisterUserRequest request);
+    void update(UpdateUserRequest request, @MappingTarget User user);
 }
