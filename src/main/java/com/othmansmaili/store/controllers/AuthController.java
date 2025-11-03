5b26c63bfffafe0e7fdb6e470cffae4dfc079fdd
Othman Smaili <othman.smaili123@gmail.com>
2025-11-02 20:51:34 -0800

    add security features
    - build login API
    - generate JWTs
    - add JWT validation

---
+++ src/main/java/com/othmansmaili/store/controllers/AuthController.java
@@ -0,0 +1,53 @@
+package com.othmansmaili.store.controllers;
+
+import com.othmansmaili.store.dtos.JwtResponse;
+import com.othmansmaili.store.dtos.LoginRequest;
+import com.othmansmaili.store.services.JwtService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/auth")
+@Tag(name = "Authentication")
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(
+        @Valid @RequestBody LoginRequest request
+    ) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+            )
+        );
+
+        var token = jwtService.generateToken(request.getEmail());
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @PostMapping("/validate")
+    public boolean validate(@RequestHeader("Authorization") String authHeader) {
+        System.out.println("Validate called");
+
+        var token = authHeader.replace("Bearer ", "");
+
+        return jwtService.validateToken(token);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
